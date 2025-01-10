5840ccc3eca85a42e0c55b13e4f178e12ef4e18f
Matheus Silva <worcx0@proton.me>
2025-01-10 10:51:52 -0300

    feat: Livro class

---
+++ src/Livro.java
@@ -0,0 +1,56 @@
+public class Livro {
+
+    private String titulo;
+    private String autor;
+    private int ano;
+    private boolean emprestado;
+
+    public Livro(String titulo, String autor, int ano) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.ano = ano;
+        this.emprestado = false;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public boolean isEmprestado() {
+        return emprestado;
+    }
+
+    public void setEmprestado(boolean emprestado) {
+        this.emprestado = emprestado;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", ano=" + ano +
+                ", emprestado=" + emprestado +
+                '}';
+    }
+}
